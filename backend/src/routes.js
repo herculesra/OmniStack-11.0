77625ad1738e9f0b2ce04aab05954cdc30517447
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-03-29 16:59:05 -0300

    cadastro e listagem de ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,38 @@
 // entidade responsável por armazenar as rotas da aplicação
+
 const express = require('express');
+const crypto = require('crypto');
+// responsavel por se conectar com o banco de dados
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana OmniStack',
-        aluno: 'Hércules Rodrigues',
-        msg: 'Hello World!'
+routes.get('/ongs', async (request, response) => {
+    // selecionando todas as ongs
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+
+});
+
+routes.post('/ongs', async (request, response) => {
+    // fazendo a desistruturação dos dados recebidos.
+    const { name, email, whatsapp, city, uf }= request.body;
+    
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    // Persistindo os dados
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,   
     });
+
+    // devolve apenas o ID, isso vai funcionar como um 'cpf' ou 'cnpj' da ong.
+    return response.json({ id });
 });
 
 module.exports = routes;
