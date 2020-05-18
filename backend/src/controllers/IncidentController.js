a4d4a7ea5f4cb658cd287d6b033ac41913b6d6dc
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-05-18 13:20:40 -0300

    Configurando CORS e editando as informações enviadas de cada ONG. Back end finalizado

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -11,9 +11,18 @@ module.exports = {
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
+        // join para trazer todas as infos da minha ong
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
-        .select('*');
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+// como está em desenvolvimento deixa apenas assim
+app.use(cors());
 // informando para nossa API que as requisições serão JSON
 app.use(express.json());
 // usando as rotas
